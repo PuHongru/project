b5614657bf31e338cc2d40ce1efc2590cd054422
PuHongru <puhongruoffer@163.com>
2016-10-28 23:51:31 +0800

    轮播图的响应式实现

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by puhongru on 2016/10/28.
+ */
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
