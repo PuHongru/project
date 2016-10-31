e2e6a7250bbf9f0212d3f7d3a98a11a9751f8cc2
PuHongru <puhongruoffer@163.com>
2016-10-31 23:30:07 +0800

    打包

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+/**
+ * Created by puhongru on 2016/10/31.
+ */
+var gulp = require('gulp');
+var rev = require("gulp-rev");
+var revReplace = require("gulp-rev-replace");
+var useref = require("gulp-useref");
+var filter = require("gulp-filter");
+var uglify = require("gulp-uglify");
+var csso = require("gulp-csso");
+
+gulp.task('default',function(){
+    var jsFilter = filter('**/*.js',{restore:true});
+    var cssFilter = filter('**/*.css',{restore:true});
+    var indexHtmlFilter = filter(['**/*','!**/index.html'],{restore:true});
+    //console.log(gulp.src('src/index.html').pipe(useref()));
+
+    return gulp.src("src/index.html")
+        .pipe(useref())
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe(csso())
+        .pipe(cssFilter.restore)
+        .pipe(indexHtmlFilter)
+        .pipe(rev())
+        .pipe(indexHtmlFilter.restore)
+        .pipe(revReplace())
+        .pipe(gulp.dest('dist'));
+});
